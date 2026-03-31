b3de9292d55a8520cb67b1aade05faa1dbfa5aa8
arch <miniyu157@163.com>
2026-03-31 09:59:43 +0800

    chore: 优化评论区外观

--- .vitepress/theme/components/MyWaline.vue
+++ .vitepress/theme/components/MyWaline.vue
@@ -1,26 +1,58 @@
 <template>
-  <ClientOnly>
-    <Waline
-      :key="path"
-      :serverURL="serverURL"
-      :path="path"
-      :meta="['nick', 'mail']"
-      :search="false"
-      :emoji="[]"
-      dark="html.dark"
-    />
-  </ClientOnly>
+  <div ref="walineContainer">
+    <ClientOnly>
+      <Waline
+        :key="path"
+        :serverURL="serverURL"
+        :path="path"
+        :search="false"
+        :emoji="[]"
+        :noCopyright="false"
+        :noRss="true"
+        dark="html.dark" />
+    </ClientOnly>
+  </div>
 </template>
 
 <script setup>
-import { Waline } from '@waline/client/component';
-import { computed } from 'vue';
-import { useRoute } from 'vitepress';
+import { Waline } from '@waline/client/component'
+import { computed, ref, onMounted, onUnmounted } from 'vue'
+import { useRoute } from 'vitepress'
 
-import '@waline/client/style';
-import '../waline-theme.css';
+import '@waline/client/style'
+import '../waline-theme.css'
 
-const serverURL = 'https://my-hrt-note-server.vercel.app';
-const route = useRoute();
-const path = computed(() => route.path.replace(/(index)?\.html$/, ''));
+const serverURL = 'https://my-hrt-note-server.vercel.app'
+const route = useRoute()
+const path = computed(() => route.path.replace(/(index)?\.html$/, ''))
+
+const walineContainer = ref(null)
+let observer = null
+
+onMounted(() => {
+  observer = new MutationObserver(() => {
+    const loginInfo = document.querySelector('.wl-login-info')
+    const editor = document.getElementById('wl-edit')
+    if (editor) {
+      if (!loginInfo) {
+        editor.placeholder = '无需登录即可讨论...\n也可以点击这里后在右下角进行登录。'
+      } else {
+        editor.placeholder = '说说你的看法！'
+      }
+    }
+  })
+
+  if (walineContainer.value) {
+    observer.observe(walineContainer.value, {
+      childList: true,
+      subtree: true,
+    })
+  }
+})
+
+onUnmounted(() => {
+  if (observer) {
+    observer.disconnect()
+  }
+})
 </script>
